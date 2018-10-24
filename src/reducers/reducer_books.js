29e00b34f46b712970134e58365b67ef0f4878d2
rishabhsairawat <rishabh.sairawat01@gmail.com>
2018-10-24 13:56:53 +0530

    creates a simple app with a list of books

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Book1', pages: 123 },
+    { title: 'Book2', pages: 89 },
+    { title: 'Book3', pages: 20 },
+    { title: 'Book4', pages: 53 },
+  ]
+}
